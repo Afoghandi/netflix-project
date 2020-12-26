03d730356c2d01541bf6985722f62c57027d89c4
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-12-26 14:08:00 +0000

    Jumbotron component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './css/App.css';
-import { Switch, Route } from 'react-router-dom';
+import jumboData from './fixtures/jumbo.json';
+
+import Jumbotron from './components/jumbotron';
 
 function App() {
-	return <h1>Hello from netflx</h1>;
+	return (
+		<Jumbotron.Container>
+			{jumboData.map((item) => (
+				<Jumbotron key={item.id} direction={item.direction}>
+					{' '}
+					<Jumbotron.Pane>
+						<Jumbotron.Title>{item.title} </Jumbotron.Title>
+						<Jumbotron.SubTitle>{item.subTitle} </Jumbotron.SubTitle>
+					</Jumbotron.Pane>
+					<Jumbotron.Pane>
+						<Jumbotron.Image src={item.image} alt={item.alt} />
+					</Jumbotron.Pane>
+				</Jumbotron>
+			))}
+		</Jumbotron.Container>
+	);
 }
 
 export default App;
