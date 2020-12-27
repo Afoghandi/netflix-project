522802473b70044e91b2f6e76be9ceb457af03d1
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-12-27 01:12:22 +0000

    Accordion component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 import { JumbotronContainer } from './containers/jumbotron';
+import { FooterContainer } from './containers/footer';
+import { FaqsContainer } from './containers/faqs';
 
 function App() {
-	return <JumbotronContainer />;
+	return (
+		<>
+			{' '}
+			<JumbotronContainer />
+			<FaqsContainer /> <FooterContainer />
+		</>
+	);
 }
 
 export default App;
