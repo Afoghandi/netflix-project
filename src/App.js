b32e0cdcd0f8e4e54da0ff4c76a060fdb5ed4e6a
Afo Oshun <afoyenyen@Afos-iMac.local>
2020-12-29 01:37:02 +0000

    logged in path and protected path

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Signin, Browse, Signup } from './pages';
 
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 function App() {
-    return ( <
-        >
-        <
-        Router >
-        <
-        Route exact path = '/browse' >
-        <
-        Browse / >
-        <
-        /Route> <
-        Route exact path = '/signin' >
-        <
-        Signin / >
-        <
-        /Route> <
-        Route exact path = '/signup' >
-        <
-        Signup / >
-        <
-        /Route> <
-        Route exact path = { ROUTES.HOME } >
-        <
-        Home / >
-        <
-        /Route> <
-        /Router> <
-        />
-    );
+	const user = null;
+	return (
+		<>
+			<Router>
+				<Switch>
+					<IsUserRedirect
+						user={user}
+						loggedInPath={ROUTES.BROWSE}
+						path={ROUTES.SIGN_IN}
+						exact
+					>
+						<Signin />
+					</IsUserRedirect>
+
+					<IsUserRedirect
+						user={user}
+						loggedInPath={ROUTES.BROWSE}
+						path={ROUTES.SIGN_UP}
+						exact
+					>
+						<Signup />
+					</IsUserRedirect>
+
+					<ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+						<Browse />
+					</ProtectedRoute>
+					<IsUserRedirect
+						user={user}
+						loggedInPath={ROUTES.BROWSE}
+						path={ROUTES.HOME}
+					>
+						<Home />
+					</IsUserRedirect>
+				</Switch>
+			</Router>{' '}
+		</>
+	);
 }
 
 export default App;
